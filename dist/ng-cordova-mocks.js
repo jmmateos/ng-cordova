1ad31af3539e4ca7c7dc79f3839df993ef5a777f
Jose Manuel Mateos <josemmll@gmail.com>
2017-04-19 16:29:33 +0200

    ngCordovaMocks.cordovaPrinter

--- dist/ng-cordova-mocks.js
+++ dist/ng-cordova-mocks.js
@@ -74,6 +74,46 @@ ngCordovaMocks.factory('$cordovaAppVersion', ['$q', function ($q) {
     }
   };
 }]);
+/**
+ * @ngdoc service
+ * @name ngCordovaMocks.cordovaPrinter
+ *
+ * @description
+ * A service for testing printer features
+ * in an app build with ngCordova.
+ **/ngCordovaMocks.factory('$cordovaPrinter', ['$q', '$window', function ($q, $window) {
+
+  var throwsError = false;
+
+    return {
+
+      throwsError: throwsError,
+
+      isAvailable: function () {
+        var defer = $q.defer();
+        if (this.throwsError) {
+          defer.reject('There was an error printing.');
+        } else {
+          defer.resolve(true);
+        }
+
+        return defer.promise;
+      },        
+
+
+      print: function (doc, options) {
+        var defer = $q.defer();
+        if (this.throwsError) {
+          defer.reject('There was an error printing.');
+        } else {
+          defer.resolve();
+        }
+
+        return defer.promise;    
+            
+      }
+    };
+  }]);
 
 /**
  * @ngdoc service
