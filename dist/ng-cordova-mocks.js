21af2b86020641f3da07df4c9c2c22895699d36b
Jose Manuel Mateos <josemmll@gmail.com>
2017-06-12 11:49:51 +0200

    build

--- dist/ng-cordova-mocks.js
+++ dist/ng-cordova-mocks.js
@@ -2331,6 +2331,36 @@ ngCordovaMocks.factory('$cordovaGooglePlayGame', ['$q', function ($q) {
 
 }]);
 
+ngCordovaMocks.factory('$cordovaImagePicker', ['$q', function ($q) {
+  var throwsError = false;
+
+  return {
+    /**
+     * @ngdoc property
+     * @name throwsError
+     * @propertyOf ngCordovaMocks.imagePicker
+     * @type Boolean
+     *
+     * @description
+     * A flag that signals whether a promise should be rejected.
+     * This property should only be used in automated tests
+     */
+    throwsError: throwsError,
+
+    getPictures: function () {
+      var defer = $q.defer();
+
+      if (this.throwsError) {
+        defer.reject('There was an error on showing action sheet');
+      } else {
+        defer.resolve(['file:///']);
+      }
+
+      return defer.promise;
+    }
+  };
+}]);
+
 /**
  * @ngdoc service
  * @name ngCordovaMocks.cordovaKeyboard
